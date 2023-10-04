ba8d3e11b568b52ca783acaa1194733a7b0d4ad2
mdlawrenceusa <mdlawrence@me.com>
2023-10-04 15:31:43 -0400

    2 files modified

--- src/app/top-bar/top-bar.component.ts
+++ src/app/top-bar/top-bar.component.ts
@@ -7,10 +7,47 @@ import { Component } from '@angular/core';
 })
 export class TopBarComponent {
 
+  title = 'Skillfair';
+
+  links = [
+    { text: 'Anthony', url: '/anthony' },
+    { text: 'Newest', url: '/newest' },
+    { text: 'Catalog', url: '/catalog' },
+    { text: 'Contact', url: '/contact' }
+  ];
+
+  constructor() {}
+
 }
 
 
 /*
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent {
+
+  title = 'Skillfair';
+
+  links = [
+    { text: 'Newest', url: '/newest' },
+    { text: 'Catalog', url: '/catalog' },
+    { text: 'Contact', url: '/contact' }
+  ];
+
+  constructor() {}
+
+}
+
+
+
+
+
+
 Copyright Google LLC. All Rights Reserved.
 Use of this source code is governed by an MIT-style license that
 can be found in the LICENSE file at https://angular.io/license
